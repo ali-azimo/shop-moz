7c8bbb7c399bf67a5fb22df2a13fef4ce8605d08
Ali Azimo <webdesign.aejl@gmail.com>
2025-06-12 12:27:54 +0200

    Adicionar Autenticar

--- src/app/Header.jsx
+++ src/app/Header.jsx
@@ -2,12 +2,13 @@
 
 import { FaSearch } from 'react-icons/fa';
 import Link from 'next/link';
+import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
+
 
 export default function Header() {
 
-  
   return (
-    <header className='bg-slate-200 shadow-md'>
+     <header className='bg-slate-200 shadow-md'>
       <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
         <Link href='/'>
           <h1 className='font-bold text-sm sm:text-xl flex flex-wrap'>
@@ -38,11 +39,16 @@ export default function Header() {
               About
             </li>
           </Link>
+          <SignedIn>
+            <UserButton />
+          </SignedIn>
+          <SignedOut>
             <Link href='/sign-in'>
               <li className='hidden md:inline text-slate-700 hover:underline'>
                 Sign In
               </li>
             </Link>
+          </SignedOut>
         </ul>
       </div>
     </header>
